714d7317739a0f6029e0f7eb324c22c597297c3f
Akili Hinson <akili.hinson@gmail.com>
2013-10-20 15:25:38 -0400

    RT 9.1.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,19 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
   		sign_in @user
       flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
-	else
-		render 'new'
-	end
+  	else
+  		render 'new'
+  	end
   end		
 
   private
